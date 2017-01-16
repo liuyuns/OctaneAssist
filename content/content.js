4cd32bc8a21765ca7c9615313de2df63f3c40b90
Easton Liu <yun-sheng.liu@hpe.com>
2017-01-16 16:01:51 +0800

    Version #1: * Enable the copy icon in toolbar when it's AGM site * Click will copy item(s) as format "#[ID]: #[Name]"

---
+++ content/content.js
@@ -0,0 +1,92 @@
+(function () {
+
+    function getAGMItemsText() {
+        function formatItem(id, name) {
+            return `#${id}: ${name}`;
+        }
+
+        var url = document.URL;
+        var id;
+        var matches = url.match(/entityId=(\d+)/);
+        if (matches) {
+            id = matches[1];
+        }
+
+        if (!!id) {
+            var title = "";
+            var h2 = document.getElementById("gwt-debug-cell-name");
+            if (h2) {
+                title = h2.textContent.trim();
+            }
+            return (id && title) ? formatItem(id, title) : "";
+        }
+
+        var selected = document.querySelectorAll('.x-grid3-row-selected');
+        var items = [];
+        for (var i = 0; i < selected.length; i++) {
+            var row = selected[i];
+            var idCol = row.querySelector('#gwt-debug-col-id-id');
+            var nameCol = row.querySelector('#gwt-debug-col-id-name');
+            if (!!idCol && !!nameCol) {
+                items.push(formatItem(idCol.textContent.trim(), nameCol.textContent.trim()));
+            }
+        }
+
+        return items.join('\r\n');
+    }
+
+
+    function prompt(text) {
+        var divId = "__agm_assist_prompt";
+
+        var remove = function () {
+            var d = document.getElementById(divId);
+            if (d) {
+                document.body.removeChild(d);
+            }
+        };
+
+        remove();
+
+        function htmlToElements(html) {
+            var template = document.createElement('template');
+            template.innerHTML = html;
+            return template.content.childNodes;
+        }
+
+        var outerDivHtml = `<div id='${divId}' style='position:fixed; margin-top:100px; z-index:10000; left:30%; right:30%; background:lightyellow; white-space:pre-line'></div>`;
+        var innerDivHtml = `<div style='text-align:left; color:green; margin: 10px; line-height:1.5; font-size: 120%'></div>`;
+
+        var outerDiv = htmlToElements(outerDivHtml)[0];
+        var innerDiv = htmlToElements(innerDivHtml)[0];
+        outerDiv.appendChild(innerDiv);
+
+        innerDiv.textContent = !!text ? `Following copied to clipboard: \n ${text}` : `Nothing copied`;
+
+        document.body.insertBefore(outerDiv, document.body.children[0]);
+
+        window.setTimeout(function () {
+            remove();
+        }, 2000);
+
+    }
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        switch (request.action) {
+            case "copy":
+                console.log("content side got copy request");
+
+                var text = getAGMItemsText();
+
+                console.log("send back: " + text);
+
+                sendResponse({ data: text });
+
+                prompt(text);
+
+                break;
+            default:
+                break;
+        }
+    });
+})();
---
+++ ext/background.js
@@ -0,0 +1,59 @@
+(
+    function () {
+        var isDebugging = false;
+        var re = /saas.hp(.*).com\/agm/;
+
+        function isAgmSite(url){
+            return re.test(url);
+        }
+
+        function onCopyClicked(tab) {
+            var re = /saas.hp(.*).com\//;
+            if (!isAgmSite(tab.url)) {
+                console.log("Nothing to copy, since the URL does not look like AGM site, URL: " + tab.url);
+                if (!isDebugging){
+                    return;
+                }
+            }
+
+            chrome.tabs.sendMessage(tab.id, { action: "copy" }, function (response) {
+                if (response && response.data) {
+                    copyText(response.data);
+                }
+            });
+        }
+
+        // show page action for tabs that matches the condition.
+        chrome.tabs.onUpdated.addListener(function (tabid, changeInfo, tab) {
+            if (!isAgmSite(tab.url)) {
+                chrome.pageAction.hide(tabid);
+                if (!isDebugging)
+                {
+                    return;
+                }
+            }
+
+            chrome.pageAction.show(tabid);
+
+            chrome.pageAction.onClicked.removeListener(onCopyClicked);
+            chrome.pageAction.onClicked.addListener(onCopyClicked);
+        });
+
+        chrome.tabs.onActivated.addListener(function(activeInfo){
+            chrome.pageAction.show(activeInfo.tabId);
+
+                        chrome.pageAction.onClicked.removeListener(onCopyClicked);
+            chrome.pageAction.onClicked.addListener(onCopyClicked);
+
+        })
+
+        function copyText(text) {
+            var input = document.createElement('textarea');
+            document.body.appendChild(input);
+            input.value = text;
+            input.focus();
+            input.select();
+            document.execCommand('Copy');
+            input.remove();
+        };
+    })();
